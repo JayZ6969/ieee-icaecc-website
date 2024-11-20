4a1cec6e4a33136924cd1bf0917d42549038ddd8
Zoroh26 <rohith26p@gmail.com>
2024-11-20 23:15:36 +0530

    carousel minor changes

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -10,14 +10,14 @@ import img4 from '../public/4.png'
 
 const classes = {
     InfoSection: 'z-10 flex-1 w-[452px] lg:-ml-[200px] w-full p-8 lg:clip-slant-left flex flex-col items-center gap-2 justify-center bg-white ',
-    logo: 'h-14 w-auto object-contain -ml-8 mt-[1px] ml-2',
-    DateLocation: 'ml-4 flex flex-row gap-4 mt-4 sm:max-width-[350px]',
-    Date: 'border mt-2 rounded-2xl text-black py-1 p-2',
-    Location: 'font-bold mt-1 text-black p-2',
-    Details: 'max-w-[450px] font-medium text-black ml-12 text-justify p-2',
-    buttons: 'flex flex-row gap-6 mt-3 mb-[1px] mb:-ml-4',
-    ContactButton: 'border text-black font-medium p-4 lg:p-3 rounded-lg ',
-    RegisterNow: 'bg-orange-400 font-medium text-white p-4 lg:p-3 rounded-lg'
+    logo: 'h-14 w-auto 2xl:h-20 object-contain -ml-8 mt-[1px] ml-2',
+    DateLocation: 'ml-4 flex  flex-row gap-4 mt-4 sm:max-width-[350px]',
+    Date: 'border mt-2 rounded-2xl 2xl:text-2xl text-black py-1 p-2',
+    Location: 'font-bold mt-1 text-black 2xl:text-2xl p-2',
+    Details: 'max-w-[450px] 2xl:max-w-[600px] font-medium 2xl:text-2xl text-black ml-12 text-justify p-2',
+    buttons: 'flex flex-row  gap-6 2xl:gap-12 mt-3 mb-[1px] mb:-ml-4',
+    ContactButton: 'border text-black 2xl:text-2xl font-medium p-4 lg:p-3 rounded-lg ',
+    RegisterNow: 'bg-orange-400 font-medium 2xl:text-2xl text-white p-4 lg:p-3 rounded-lg'
 }
 
 const Carousel = () => {
@@ -40,15 +40,15 @@ const Carousel = () => {
     };
 
     return (
-        <div className='flex flex-row w-full rounded-2xl pt-[7.7rem]'>
-            <div className="relative min-w-[850px] flex-1 overflow-hidden hidden lg:block">
+        <div className='flex flex-row w-full rounded-2xl pt-[7.6rem]'>
+            <div className="relative flex-1 overflow-hidden hidden lg:block">
                 <div className="flex transition-transform duration-500 ease-in-out" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
                     {images.map((src, index) => (
-                        <Image key={index} src={src} className="w-full max-h-[450px] object-contain " alt={`Slide ${index + 1}`} />
+                        <Image key={index} src={src} className="w-full min-h-[450px] object-cover " alt={`Slide ${index + 1}`} />
                     ))}
                 </div>
                 <button className="absolute top-1/2 transform -translate-y-1/2 bg-white rounded-full bg-opacity-50 text-white border-none p-2 py-0 cursor-pointer left-2 z-20" onClick={prevSlide}>&#10094;</button>
-                <button className="absolute top-1/2 transform -translate-y-1/2 bg-black rounded-full bg-opacity-50 text-white border-none p-2 py-0 cursor-pointer right-2 lg:right-20 z-20 hidden xl:block " onClick={nextSlide}>&#10095;</button>
+                <button className="absolute top-1/2 transform -translate-y-1/2 bg-black rounded-full bg-opacity-50 text-white border-none p-2 py-0 cursor-pointer right-2 md:right-16 2xl:right-12 z-20 hidden xl:block " onClick={nextSlide}>&#10095;</button>
             </div>
             <div className={classes.InfoSection}>
                 <div className=' lg:-mr-[70px] flex flex-col justify-center items-center gap-2'>
